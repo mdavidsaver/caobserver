52286c59e108b43f0606ce78da7b5782bf9454be
Michael Davidsaver <mdavidsaver@gmail.com>
2015-01-24 12:36:02 -0500

    set python path automatically for wsgi

--- web/caobserver/wsgi.py
+++ web/caobserver/wsgi.py
@@ -7,8 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "caobserver.settings")
-
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
